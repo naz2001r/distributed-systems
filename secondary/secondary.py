14f3f449d3ad84d762d945b34363925b527b470c
Marianna Kovalova <marisabelka22@gmail.com>
2022-11-19 17:45:34 +0200

    Added logging

--- secondary/secondary.py
+++ secondary/secondary.py
@@ -86,16 +86,18 @@ class Secondary:
     def _store_data(self, data, data_number):
         if len(self.data_storage) + 1 == data_number:
             self.data_storage.append(data)
-            logging.info(f'Received message:{data}')
+            logging.info(f'Received message: {data}')
             num = len(self.data_storage) + 1
             while num in self.temp_data_storage.keys():
                 self.data_storage.append(self.temp_data_storage[num])
                 del self.temp_data_storage[num]
                 num += 1
+                logging.info(f"Got message '{data}' from temporary storage")
         elif len(self.data_storage) <= data_number:
             logging.info(f"Received duplicate message: {data}")
         else:
             self.temp_data_storage[len(self.data_storage) + 1] = data
+            logging.info(f"Added message '{data}' to temporary storage")
 
     def _make_artificial_delay(self):
         delay_probability = random.uniform(0, 1)
